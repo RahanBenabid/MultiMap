6d50002e66f898c8f92b2df65661e46d3fc3788b
RahanBenabid <rahannadime@gmail.com>
2024-06-27 16:32:47 +0100

    Initial Commit

---
+++ MultiMap/MultiMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by Rahan Benabid on 27/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiMapTests/MultiMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  MultiMapTests.swift
+//  MultiMapTests
+//
+//  Created by Rahan Benabid on 27/6/2024.
+//
+
+import Testing
+@testable import MultiMap
+
+struct MultiMapTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
